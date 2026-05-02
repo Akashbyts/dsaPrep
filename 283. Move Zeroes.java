fa9db6350d3709689ec4a4865221fbb44427bc30
Akash Mandal <djakashmandal@gmail.com>
2026-05-03 01:57:15 +0530

    Add files via upload

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        int ptr = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                temp[ptr] = nums[i];
+                ptr++;
+            }
+        }
+        for(int i=ptr;i<n;i++){
+            temp[i] = 0;
+        }
+         for(int i=0;i<n;i++){
+            nums[i] = temp[i];
+         }
+    }
+}
